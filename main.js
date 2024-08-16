9df5eebac225160d3d00dc4adecc49edd2f997e9
codingwithtim73 <91760043+codingwithtim73@users.noreply.github.com>
2024-08-16 10:26:41 +1000

    changed imports in main.js

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-import {
-  ClassicEditor,
-  AccessibilityHelp,
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage";
+import AutoLink from "@ckeditor/ckeditor5-link/src/autolink";
+import Autosave from "@ckeditor/ckeditor5-autosave/src/autosave";
+import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import CloudServices from "@ckeditor/ckeditor5-cloud-services/src/cloudservices";
+import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock";
+import FindAndReplace from "@ckeditor/ckeditor5-find-and-replace/src/findandreplace";
+import GeneralHtmlSupport from "@ckeditor/ckeditor5-html-support/src/generalhtmlsupport";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline";
+import HtmlComment from "@ckeditor/ckeditor5-html-support/src/htmlcomment";
+import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed";
+import ImageBlock from "@ckeditor/ckeditor5-image/src/imageblock";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+import ImageInline from "@ckeditor/ckeditor5-image/src/imageinline";
+import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert";
+import ImageInsertViaUrl from "@ckeditor/ckeditor5-image/src/imageinsertviaurl";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageTextAlternative from "@ckeditor/ckeditor5-image/src/imagetextalternative";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage";
+import PageBreak from "@ckeditor/ckeditor5-page-break/src/pagebreak";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import SelectAll from "@ckeditor/ckeditor5-select-all/src/selectall";
+import ShowBlocks from "@ckeditor/ckeditor5-show-blocks/src/showblocks";
+import SourceEditing from "@ckeditor/ckeditor5-source-editing/src/sourceediting";
+import Style from "@ckeditor/ckeditor5-style/src/style";
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TableCaption from "@ckeditor/ckeditor5-table/src/tablecaption";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableColumnResize from "@ckeditor/ckeditor5-table/src/tablecolumnresize";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+import Undo from "@ckeditor/ckeditor5-undo/src/undo";
+
+ClassicEditor.builtinPlugins = [
+  Essentials, // Includes AccessibilityHelp implicitly
   Alignment,
   Autoformat,
   AutoImage,
@@ -11,7 +58,6 @@ import {
   Bold,
   CloudServices,
   CodeBlock,
-  Essentials,
   FindAndReplace,
   GeneralHtmlSupport,
   Heading,
@@ -47,89 +93,42 @@ import {
   TableToolbar,
   TextTransformation,
   Undo,
-} from "ckeditor5";
+];
 
-export default ClassicEditor.create(document.querySelector("#editor"), {
-  plugins: [
-    AccessibilityHelp,
-    Alignment,
-    Autoformat,
-    AutoImage,
-    AutoLink,
-    Autosave,
-    Base64UploadAdapter,
-    BlockQuote,
-    Bold,
-    CloudServices,
-    CodeBlock,
-    Essentials,
-    FindAndReplace,
-    GeneralHtmlSupport,
-    Heading,
-    HorizontalLine,
-    HtmlComment,
-    HtmlEmbed,
-    ImageBlock,
-    ImageCaption,
-    ImageInline,
-    ImageInsert,
-    ImageInsertViaUrl,
-    ImageResize,
-    ImageStyle,
-    ImageTextAlternative,
-    ImageToolbar,
-    ImageUpload,
-    Indent,
-    IndentBlock,
-    Italic,
-    Link,
-    LinkImage,
-    PageBreak,
-    Paragraph,
-    SelectAll,
-    ShowBlocks,
-    SourceEditing,
-    Style,
-    Table,
-    TableCaption,
-    TableCellProperties,
-    TableColumnResize,
-    TableProperties,
-    TableToolbar,
-    TextTransformation,
-    Undo,
-  ],
-  toolbar: [
-    "heading",
-    "|",
-    "bold",
-    "italic",
-    "underline",
-    "strikethrough",
-    "subscript",
-    "superscript",
-    "code",
-    "link",
-    "|",
-    "bulletedList",
-    "numberedList",
-    "blockquote",
-    "imageUpload",
-    "insertTable",
-    "mediaEmbed",
-    "alignment",
-    "fontSize",
-    "fontColor",
-    "fontBackgroundColor",
-    "|",
-    "codeBlock",
-    "horizontalLine",
-    "specialCharacters",
-    "|",
-    "removeFormat",
-    "undo",
-    "redo",
-  ],
+ClassicEditor.defaultConfig = {
+  toolbar: {
+    items: [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "strikethrough",
+      "subscript",
+      "superscript",
+      "code",
+      "link",
+      "|",
+      "bulletedList",
+      "numberedList",
+      "blockquote",
+      "imageUpload",
+      "insertTable",
+      "mediaEmbed",
+      "alignment",
+      "fontSize",
+      "fontColor",
+      "fontBackgroundColor",
+      "|",
+      "codeBlock",
+      "horizontalLine",
+      "specialCharacters",
+      "|",
+      "removeFormat",
+      "undo",
+      "redo",
+    ],
+  },
   image: {
     toolbar: [
       "imageStyle:full",
@@ -141,4 +140,6 @@ export default ClassicEditor.create(document.querySelector("#editor"), {
   table: {
     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
   },
-});
+};
+
+export default ClassicEditor;
